17aaaf58d473ab86c491558b721e844ef60e19bf
cowboykipnugget <524877@vistacollege.nl>
2025-02-11 14:09:37 +0100

    hgds

--- website/script.js
+++ website/script.js
@@ -8,15 +8,19 @@
             if (item === 'Broodjes') {
                 title = "Broodje gezond";
                 image = "https://thumbs.dreamstime.com/b/submarine-sandwich-ham-cheese-clipping-path-egg-tomato-cucumber-lettuce-white-48580716.jpg";
-                description = "Broodje met ei en sla ectr.";
+                description = "Broodje met ei en sla etc.";
             } else if (item === 'drinks') {
                 title = "Spa water";
                 image = "https://m.media-amazon.com/images/I/71VrczELeaL._AC_SY741_.jpg";
-                description = "Koude verfrissende watuuur ectr.";
-            } else if (item === 'Snacks') {
+                description = "Koude verfrissende watuuur etc.";
+            } else if (item === 'ijsjes') {
                 title = "Frikandel";
                 image = "https://boshuis.huisjebezorgd.nl/wp-content/uploads/2020/03/29512948_652505005141152_1601506864166600704_o.jpg"; 
                 description = "vlees etc.";
+            }  else if (item === 'ijsjes') {
+                title = "deserts";
+                image = "https://png.pngtree.com/png-clipart/20230411/original/pngtree-ice-cream-dessert-realistic-white-background-transparent-png-image_9047252.png"; 
+                description = "ijsje etc.";
             }
 
             // Toon de details aan de rechterkant
