c129bb344996f6c9521e0706eddea2261501bc22
vista-man <524715@vistacollege.nl>
2025-02-12 10:02:47 +0100

    Update image sources for deals and soup in item details

--- website/script.js
+++ website/script.js
@@ -21,11 +21,11 @@ function showItemDetails(item) {
                 description = "Een lekker ijsje met vele smaken, zoals aardbei, vannille, chocolade, mint, bosbes en nog veel meer (alleen in de zomer!)";
             }   else if (item === 'Deals') {
                 title = "Deals";
-                imageSrc = "https://archies.com/wp-content/uploads/2024/07/MEAL-DEALS_HERO_MOBILE-1.jpg"; 
+                imageSrc = "media/deals.jpg"; 
                 description = "Onze beste deals met de beste prijzen!";
             }   else if (item === 'soep') {
                 title = "soepen";
-                imageSrc = "https://www.lekkeretenmetlinda.nl/wp-content/uploads/2025/01/Minestrone-soep-1a-768x576.jpg";
+                imageSrc = "media/soep.jpg";
                 description = "soep van de dag! (Allergieen? Meld het bij ons!";
             }   else if (item === 'Placeholder 2') {
                 title = "Placeholder 2";
