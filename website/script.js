7c129355886d8da5c33f58d71f07f94633b7c269
cowboykipnugget <524877@vistacollege.nl>
2025-02-12 10:08:46 +0100

    sf

--- website/script.js
+++ website/script.js
@@ -6,19 +6,19 @@ function showItemDetails(item) {
              // Stel de inhoud in afhankelijk van het geselecteerde item
     if (item === 'Broodjes') {
         title = "Broodje Gezond";
-        image = "https://thumbs.dreamstime.com/b/submarine-sandwich-ham-cheese-clipping-path-egg-tomato-cucumber-lettuce-white-48580716.jpg";
+        imagesrc = "broodje-gezond.jpg";
         description = "Op dit broodje zit kaas, veldsla, komkommer, tomaat, ei, ham en/of kip en bufkes saus.";
     } else if (item === 'drinks') {
         title = "Spa Water";
-        image = "https://m.media-amazon.com/images/I/71VrczELeaL._AC_SY741_.jpg";
+        imagesrc = "spa.jpg";
         description = "Koude verfrissende water.";
     } else if (item === 'Snacks') {
         title = "Frikandel";
-        image = "https://boshuis.huisjebezorgd.nl/wp-content/uploads/2020/03/29512948_652505005141152_1601506864166600704_o.jpg";
+        imagesrc = "frikandel.jpg";
         description = "Een frikandel, dat wil je wel!";
     } else if (item === 'deserts') {
         title = "Ijsjes";
-        image = "https://th.bing.com/th/id/R.5e81f369a0da6a92106391e7b0a5119b?rik=KD0kNGiRjGM22g&riu=http%3a%2f%2fwww.brookersicecream.com%2fwp-content%2fuploads%2f2018%2f09%2fIce-Cream-Fan.png&ehk=%2bfZOURa%2fKgmE4uuuESx9We3OBrwXpPaaMZpJaBJt4bo%3d&risl=&pid=ImgRaw&r=0";
+        imagesrc = "ijs.png";
         description = "Een lekker ijsje met vele smaken, zoals aardbei, vanille, chocolade, mint, bosbes en nog veel meer (alleen in de zomer!).";
     }   else if (item === 'Deals') {
                 title = "Deals";
