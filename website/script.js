f518707663c7a21b30cf3861a6786b9d032e87ce
vista-man <524715@vistacollege.nl>
2025-02-13 11:52:45 +0100

    edit

--- website/script.js
+++ website/script.js
@@ -37,7 +37,7 @@ function showCategory(category) {
     else if (category === 'Snacks') {
         items = [
             { title: "Frikandel", imageSrc: "media/frikandel.jpg", price: 2.00 },
-            { title: "Bitterballen", imageSrc: "https://www.taalvoutjes.nl/app/uploads/2014/09/bitterbal.jpg", price: 2.50 },
+            { title: "Bitterballen", imageSrc: "media/bitterbal.jpg", price: 2.50 },
             { title: "Mexicano", imageSrc: "media/mexicano.png", price: 2.00 },
             { title: "Kipcorn", imageSrc: "media/kipcorn.png", price: 2.20 },
             { title: "Friet", imageSrc: "media/friet.png", price: 3.20 },
@@ -56,9 +56,9 @@ function showCategory(category) {
     } else if (category === 'Soepen') {
         items = [
             { title: "Tomatensoep", imageSrc: "media/soep.jpg", price: 2.50 },
-            { title: "Kippensoep", imageSrc: "https://static.ah.nl/static/recepten/img_RAM_PRD121479_1224x900_JPG.jpg", price: 2.80 },
-            { title: "Erwtensoep", imageSrc: "https://www.24kitchen.nl/files/styles/media_text_wide/public/2022-09/erwtensoep%20%282%29.webp?itok=5FwzHgSE", price: 2.50 },
-            { title: "Groentesoep (met gehaktballetjes)", imageSrc: "https://www.maggi.nl/sites/default/files/styles/home_stage_1500_700/public/srh_recipes/a55f0226c938b6bc40878dad5306271b.jpg?h=02cb7f90&itok=AmnkjPc3", price: 2.80 },
+            { title: "Kippensoep", imageSrc: "media/kippensoep.jpg", price: 2.80 },
+            { title: "Erwtensoep", imageSrc: "media/erwtensoep.webp", price: 2.50 },
+            { title: "Groentesoep (met gehaktballetjes)", imageSrc: "media/groentesoep.jpg", price: 2.80 },
         ];
     } else if (category === 'Salades') {
         items = [
