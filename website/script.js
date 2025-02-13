462cd5f20637473b007ed310e395bb36c32ff44b
Dylanomz <527457@vistacollege.nl>
2025-02-13 12:20:21 +0100

    k

--- website/script.js
+++ website/script.js
@@ -124,7 +124,3 @@ function getDescription(title) {
     return "";
 }
 
-// Functie om het modaal venster te sluiten
-function closeModal() {
-    document.getElementById('modal').style.display = 'none';
-}
