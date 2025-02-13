d9c2ebea4642c6b1c2ad00b52a8fe689070ac68e
Dylanomz <527457@vistacollege.nl>
2025-02-13 11:32:02 +0100

    Update script.js

--- website/script.js
+++ website/script.js
@@ -44,6 +44,8 @@ function showCategory(category) {
         items = [
             { title: "Frikandel", imageSrc: "media/frikandel.jpg", price: 2.00 },
             { title: "Bitterballen", imageSrc: "https://www.taalvoutjes.nl/app/uploads/2014/09/bitterbal.jpg", price: 2.50 },
+            { title: "Mexicano", imageSrc: "media/mexicano.png", price: 2.00 },
+            { title: "Kipcorn", imageSrc: "media/kipcorn.png", price: 2.50 },
         ];
     } else if (category === 'deserts') {
         items = [
