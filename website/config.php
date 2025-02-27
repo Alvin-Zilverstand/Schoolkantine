01f98ea7d0d489b62728c7ca4abee1d86e0228d3
vista-man <524715@vistacollege.nl>
2025-02-27 13:45:48 +0100

    Refactor database connection: remove unused port parameter and delete obsolete database.inc.php

--- website/config.php
+++ website/config.php
@@ -3,10 +3,9 @@ $servername = "localhost:3306";
 $username = "database";
 $password = "13cAv?i52";
 $dbname = "schoolkantine";
-$port = 3306; // Specify the port
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
